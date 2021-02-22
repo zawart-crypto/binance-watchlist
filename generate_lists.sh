44fb415388ec6c559cc35bf281221c2c360cf4ee
zawart-crypto <79408920+zawart-crypto@users.noreply.github.com>
2021-02-22 22:38:20 +0100

    cleanup

--- generate_lists.sh
+++ generate_lists.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 coingecko_api='https://api.coingecko.com/api/v3/exchanges/binance/tickers?page='
+tickers="tickers.txt"
 
-max_page=$(curl --silent -IX GET "${coingecko_api}1" -H "accept: application/json" | grep 'link:' \
-	| sed -e 's/>; rel="last".*//g' | sed -e 's/.*page=//g')
+last_page=$(curl --silent -IX GET "${coingecko_api}1" -H 'accept: application/json' | grep 'link:' \
+	| sed 's/>.*//g' | sed 's/.*page=//g')
 
-for((i=1;i<=${max_page};i++));do
-  curl --silent -X GET "${coingecko_api}${i}" -H "accept: application/json" | jq .[] | tail -n +2 > $i
-  cat $i | jq '.[] | select( .is_stale == false and .is_anomaly == false) .trade_url' \
-	  | sed -e 's/.*trade\///g' | sed -e 's/"//g' | sed -e 's/_//g' >> tickers
-  rm ${i}
+for((i=1;i<=${last_page};i++)); do
+  curl --silent -X GET "${coingecko_api}${i}" -H 'accept: application/json' | jq .[] | tail -n +2 \
+	  | jq '.[] | select(.is_stale == false and .is_anomaly == false) .trade_url' \
+	  | sed 's/.*trade\///g' | sed 's/"//g' | sed 's/_//g' >> symbols
 done
 
-cat tickers | xargs -n1 echo -e 'BINANCE:' | tr -d '[:blank:]' | sort > binance_tickers.txt
-rm tickers
+cat symbols | xargs -n1 echo -e 'BINANCE:' | tr -d '[:blank:]' | sort > "${tickers}"
+rm symbols
 
-cat binance_tickers.txt | grep USDT$ > binance_tickers_USDT.txt
-cat binance_tickers.txt | grep BUSD$ > binance_tickers_BUSD.txt
-cat binance_tickers.txt | grep BTC$ > binance_tickers_BTC.txt
+cat "${tickers}" | grep USDT$ > binance_markets_USDT.txt
+cat "${tickers}" | grep BUSD$ > binance_markets_BUSD.txt
+cat "${tickers}" | grep BTC$  > binance_markets_BTC.txt
